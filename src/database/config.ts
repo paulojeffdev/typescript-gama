3494cd2248eb5d7b0d2c8ac6049a7096c6a780a8
Paulo Jefferson Mendes Oliveira <paulojeff.oliveira@gmail.com>
2023-02-14 20:02:20 -0300

    Alter mongo connection and add command with postinstall

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,5 +1,6 @@
 export default {
-    uri: "mongodb://127.0.0.1:27017/account",
-    port: "27017",
-    host: "localhost"
+    uri: "mongodb://mongo:nGwXwRJ2yuZFCa5wLQpe@containers-us-west-108.railway.app:6022",
+    port: "6022",
+    host: "containers-us-west-108.railway.app",
+    user: "mongo"
 }
